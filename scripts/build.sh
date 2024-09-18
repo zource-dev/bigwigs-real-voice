84401b5d6d058d0c1887086fb7824ed141bccbc1
Ivan Zakharchanka <3axap4eHko@gmail.com>
2024-09-17 22:09:10 -0400

    v11.0.2

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-rm -rf tmp BigWigs_VoiceGCP BigWigs_VoiceGCP.zip
-git clone https://github.com/BigWigsMods/BigWigs_Voice.git tmp/BigWigs_Voice
+#rm -rf tmp BigWigs_VoiceGCP BigWigs_VoiceGCP.zip
+#git clone https://github.com/BigWigsMods/BigWigs_Voice.git tmp/BigWigs_Voice
 
 mkdir -p BigWigs_VoiceGCP/Sounds/
 node --loader ts-node/esm --no-warnings --experimental-specifier-resolution=node tts/index.ts tmp/BigWigs_Voice/Tools sounds SO
